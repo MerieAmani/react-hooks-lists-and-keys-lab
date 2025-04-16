62899e7896997871cefeb3496430defc1e8f4c94
matthew <meriematthew01@gmail.com>
2025-04-16 17:50:59 +0300

    adds lists and keys lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,37 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import ProjectList from "./components/ProjectList";
+
+const projects = [
+  {
+    id: 1,
+    name: "Project 1",
+    about: "This is project 1",
+    technologies: ["React", "JavaScript", "CSS"],
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    about: "This is project 2",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    id: 3,
+    name: "Project 3",
+    about: "This is project 3",
+    technologies: ["Node.js", "Express", "MongoDB"],
+  },
+];
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home />
+      <About />
+      <ProjectList projects={projects} />
     </div>
   );
 }
